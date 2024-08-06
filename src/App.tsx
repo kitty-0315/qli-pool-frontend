e2f7568895c47c4538f3fa0d07d888702be679d0
kitty-0315 <tarispltok.tp@gmail.com>
2024-08-07 03:13:21 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div className="w-full min-h-screen h-full flex justify-center items-center">
+        <h1 className="text-5xl">qli pool Front</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
